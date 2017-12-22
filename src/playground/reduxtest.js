1ba5cbfa7c4a083b5d38486f2b045837d2688b8e
Ahmad <ali333291@gmail.com>
2017-12-22 15:20:47 +0500

    dispatch actions

--- src/playground/reduxtest.js
+++ src/playground/reduxtest.js
@@ -1,8 +1,42 @@
 import {createStore} from 'redux'; 
-console.log('test');
 
-const store = createStore((state = {count: 0}) => {
-  return state;
+const store = createStore((state = {count: 0}, action) => {
+  switch (action.type){
+    case 'RESET':
+      return {
+        count: 0
+      };
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    
+    case 'DECREMENT':
+      return {
+        count: state.count -1 
+      };
+
+    default:
+      return state;
+  }
+});
+
+console.log(store.getState());
+
+//////////////////// ACTIONS //////////////////
+// INCREMENT
+store.dispatch({
+  type: 'INCREMENT'
+});
+
+// DECREMENT
+store.dispatch({
+  type: 'DECREMENT'
+});
+
+// RESET
+store.dispatch({
+  type: 'RESET'
 });
 
 console.log(store.getState());
