d30d09a1fd4c32771ce49024edec264519b43085
Ahmad <ali333291@gmail.com>
2018-01-19 14:10:02 +0500

    creating seperate database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyCTfJjzZyHj3J0k5d8s7VW-AoaTLO-FVeE",
-  authDomain: "expensify-7c149.firebaseapp.com",
-  databaseURL: "https://expensify-7c149.firebaseio.com",
-  projectId: "expensify-7c149",
-  storageBucket: "expensify-7c149.appspot.com",
-  messagingSenderId: "366810245393"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
