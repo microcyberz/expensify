c28a42cee1b606fbf0e8e0e3712901a755b46f37
Ahmad <ali333291@gmail.com>
2018-01-26 20:10:26 +0500

    Set  Logout

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -5,3 +5,9 @@ export const startLogin = () => {
     firebase.auth().signInWithPopup(googleAuthProvider);
   }
 }
+
+export const startLogout = () => {
+  return () => {
+    return firebase.auth().signOut();
+  }
+}
--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -2,13 +2,17 @@ import React from 'react';
 // import ReactShallowRenderer from 'react-test-renderer/shallow';
 import {shallow} from 'enzyme';
 import toJSON from 'enzyme-to-json';
-import Header from '../../components/Header';
+import {Header} from '../../components/Header';
 
 test('should render header correctly', () => {
-  const wrapper = shallow(<Header />);
+  const wrapper = shallow(<Header startLogout={ () => { } }/>);
   expect(toJSON(wrapper)).toMatchSnapshot();  
-  // expect(wrapper.find('h1').length).toBe(1);
-  // const renderer = new ReactShallowRenderer();
-  // renderer.render(<Header />);
-  // expect(renderer.getRenderOutput()).toMatchSnapshot();
 }); 
+
+
+test('should call statLogout on button click', () => {
+  const startLogout = jest.fn();
+  const wrapper = shallow(<Header startLogout={startLogout} />);
+  wrapper.find('button').simulate('click');
+  expect(startLogout).toHaveBeenCalled();
+})
